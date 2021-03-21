f68d57de26c9f047a6dd066c65637413b0da4df8
Konstantin Morozov <morozov-kst@yandex.ru>
2021-03-21 08:15:36 +0300

    fix test's output

--- test/blockTest.cpp
+++ test/blockTest.cpp
@@ -15,8 +15,22 @@ protected:
     std::ostringstream sout;
 };
 
-TEST_F(BlockTest, exampleFromTask) {
+TEST_F(BlockTest, exampleFromTaskDemo) {
     std::string text = "cmd1\ncmd2\n{\ncmd3\ncmd4\n}\n{\ncmd5\ncmd6\n{\ncmd7\ncmd8\n}\ncmd9\n}\n";
+    std::istringstream sin(text);
+
+    Settings settings(sin, sout, sout, count);
+    auto bulkPtr = std::make_unique<Bulk>(settings);
+    bulkPtr->run();
+
+    const std::string expected = "bulk: cmd1 cmd2\nbulk: cmd3 cmd4\nbulk: cmd5 cmd6 cmd7 cmd8 cmd9\n";
+    const std::string result = sout.str();
+
+    ASSERT_TRUE(result.compare(expected));
+}
+
+TEST_F(BlockTest, exampleFromTask) {
+    std::string text = "cmd1\ncmd2\n{\ncmd3\ncmd4\n}\n{\ncmd5\ncmd6\n{\ncmd7\ncmd8\n}\ncmd9\n}\ncmd10\ncmd11\n";
     std::istringstream sin(text);
 
     Settings settings(sin, sout, sout, count);
